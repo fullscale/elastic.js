491efc44efdc3d2bb35adaff0b2f9fde7642d3f8
Matt Weber <matt@mattweber.org>
2012-11-21 13:41:36 -0800

    Cleanup angular client to pass lint tests.
    Fix indentation, add global jshint parameters for angular and browser
    variables.

--- src/clients/elastic-angular-client.js
+++ src/clients/elastic-angular-client.js
@@ -1,3 +1,5 @@
+/*jshint browser:true */
+/*global angular:true */
 'use strict';
 
 /* 
@@ -5,64 +7,70 @@ Angular.js service wrapping the elastic.js API. This module can simply
 be injected into your angular controllers. 
 */
 angular.module('elasticjs.service', [])
-    .factory('ejsResource', function($http) {
+  .factory('ejsResource', function ($http) {
 
-        function ejsResourceFactory(url) {
-        
-            var ejs = window.ejs || {};
-            url = url || '';
-        
-            /* results are returned as a promise */
-            var promiseThen = function (httpPromise, successcb, errorcb) {
-                return httpPromise.then(function (response) {
-                    (successcb || angular.noop)(response.data);
-                    return response.data;
-                }, function(response) {
-                    (errorcb || angular.noop)(undefined);
-                    return undefined;
-                });
-            };
- 
-            /* implement the elastic.js client interface for angular */
-            ejs.client = {
-                server: function (s) {
-                    if (s === null) {
-                        return url;
-                    } else {
-                        url = s;
-                    }
-                    return this;
-                },
-                post: function (path, data, successcb, errorcb) {
-                    path = url + path;
-                    return promiseThen($http.post(path, data), successcb, errorcb);
-                },
-                get: function (path, data, successcb, errorcb) {
-                    path = url + path;
-                    return promiseThen($http.get(path, data), successcb, errorcb);
-                },
-                put: function (path, data, successcb, errorcb) {
-                    path = url + path;
-                    return promiseThen($http.put(path, data), successcb, errorcb);
-                },
-                del: function (path, data, successcb, errorcb) {
-                    path = url + path;
-                    return promiseThen($http.delee(path, data), successcb, errorcb);
-                },
-                head: function (path, data, successcb, errorcb) {
-                    path = url + path;
-                    return $http.head(path, data)
-                        .then(function (response) {
-                            (successcb || angular.noop)(response.headers());
-                            return response.headers();
-                        }, function (response) {
-                            (errorcb || angular.noop)(undefined);
-                            return undefined;
-                        });
-                }
-            };
-            return ejs;
+  return function (url) {
+
+    var
+
+      // use existing ejs object if it exists
+      ejs = window.ejs || {},
+
+      /* results are returned as a promise */
+      promiseThen = function (httpPromise, successcb, errorcb) {
+        return httpPromise.then(function (response) {
+          (successcb || angular.noop)(response.data);
+          return response.data;
+        }, function (response) {
+          (errorcb || angular.noop)(undefined);
+          return undefined;
+        });
+      };
+
+    // set url to empty string if it was not specified
+    if (url == null) {
+      url = '';
+    }
+
+    /* implement the elastic.js client interface for angular */
+    ejs.client = {
+      server: function (s) {
+        if (s == null) {
+          return url;
         }
-        return ejsResourceFactory;
-    });
-
+      
+        url = s;
+        return this;
+      },
+      post: function (path, data, successcb, errorcb) {
+        path = url + path;
+        return promiseThen($http.post(path, data), successcb, errorcb);
+      },
+      get: function (path, data, successcb, errorcb) {
+        path = url + path;
+        return promiseThen($http.get(path, data), successcb, errorcb);
+      },
+      put: function (path, data, successcb, errorcb) {
+        path = url + path;
+        return promiseThen($http.put(path, data), successcb, errorcb);
+      },
+      del: function (path, data, successcb, errorcb) {
+        path = url + path;
+        return promiseThen($http.delee(path, data), successcb, errorcb);
+      },
+      head: function (path, data, successcb, errorcb) {
+        path = url + path;
+        return $http.head(path, data)
+          .then(function (response) {
+          (successcb || angular.noop)(response.headers());
+          return response.headers();
+        }, function (response) {
+          (errorcb || angular.noop)(undefined);
+          return undefined;
+        });
+      }
+    };
+  
+    return ejs;
+  };
+});
