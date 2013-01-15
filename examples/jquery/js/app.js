76899705e771b804b2d363eb2136b4f1791d3d7b
Matt Weber <matt@mattweber.org>
2013-01-15 13:20:06 -0800

    Add button to index sample docs to examples
    Update angular and jquery examples with a button to index some sample
    documents.

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -14,7 +14,9 @@
       viewport = $('#viewport'),
       
       // setup the indices and types to search across
-      request = ejs.Request({indices: 'twitter', types: 'tweet'}),
+      index = 'twitter',
+      type = 'tweet',
+      request = ejs.Request({indices: index, types: type}),
       
       // generates the elastic.js query and executes the search
       executeSearch = function (qstr) {
@@ -41,8 +43,47 @@
           .click(function () {
             gotoSearch();
           });
+      },
+      
+      // index sample documents
+      indexSampleDocs = function () {
+
+        // our example documents
+        var docs = [
+          ejs.Document(index, type, '1').source({
+            user: 'mrweber', 
+            message: 'Elastic.js - a Javascript implementation of the ElasticSearch Query DSL and Core API'}), 
+      	  ejs.Document(index, type, '2').source({
+      	    user: 'egaumer',
+      	    message: 'FullScale Labs just released Elastic.js go check it out!'
+      	  }),
+          ejs.Document(index, type, '3').source({
+            user: 'dataintensive',
+            message: 'We are pleased to announce Elastic.js an implementation of the #elasticsearch query dsl'
+          }),
+          ejs.Document(index, type, '4').source({
+            user: 'kimchy',
+            message: 'The FullScale Labs team are awesome!  Go check out Elastic.js'
+          }),
+          ejs.Document(index, type, '5').source({
+            user: 'egaumer',
+            message: 'Use elastic.js to write a complex query and translate it to json with our query translator'
+          })
+        ];
+
+        // so search is only executed after all documents have been indexed
+        var doSearch = _.after(docs.length, function () {
+          executeSearch('');
+        });
+        
+        _.each(docs, function (doc) {
+          doc.refresh(true).doIndex(doSearch);
+        });
       };
       
+      // link our index docs button
+      $('#btnIndexDocs').click(indexSampleDocs);
+       
       // load the search page as the landing page
       gotoSearch();
   });
