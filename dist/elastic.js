9c17db35265c3abb6e8618cbe27e6b515a891cb6
ErwanPigneul <erwan.pigneul@gmail.com>
2015-08-02 22:49:31 +0200

    mfyang pr #1

--- dist/elastic.js
+++ dist/elastic.js
@@ -1,4 +1,4 @@
-/*! elastic.js - v1.2.0 - 2015-08-02
+/*! elastic.js - v1.3.0 - 2015-08-02
  * https://github.com/fullscale/elastic.js
  * Copyright (c) 2015 FullScale Labs, LLC; Licensed MIT */
 
@@ -12138,23 +12138,14 @@
     @param {String} likeText The text to find documents like it.
   
      */
-  ejs.MoreLikeThisQuery = function (fields, likeText) {
+  ejs.MoreLikeThisQuery = function (likeText) {
 
     var 
       _common = ejs.QueryMixin('mlt'),
       query = _common.toJSON();
     
     query.mlt.like_text = likeText;
-    query.mlt.fields = [];
 
-    if (isString(fields)) {
-      query.mlt.fields.push(fields);
-    } else if (isArray(fields)) {
-      query.mlt.fields = fields;
-    } else {
-      throw new TypeError('Argument must be string or array');
-    }
-    
     return extend(_common, {
   
       /**
@@ -12168,20 +12159,29 @@
              */
       fields: function (f) {
         if (f == null) {
-          return query.mlt.fields;
+          return this;
         }
     
         if (isString(f)) {
-          query.mlt.fields.push(f);
+          query.mlt.fields = [f];
         } else if (isArray(f)) {
           query.mlt.fields = f;
         } else {
-          throw new TypeError('Argument must be a string or array');
+          throw new TypeError('Must pass a field or an array of fields');
         }
     
         return this;
       },
-  
+
+      docs: function(doc) {
+        if (isArray(doc)) {
+          query.mlt.docs = doc;
+        } else {
+          throw new TypeError('Must pass an array of docs as argument');
+        }
+        return this;
+      },
+
       /**
             The text to find documents like
 
@@ -16398,6 +16398,26 @@
         }
 
         query.filter = filter.toJSON();
+        return this;
+      },
+
+      /**
+            Allows you to set a specified post_filter on this request object.
+
+            @member ejs.Request
+            @param {Object} filter Any valid <code>Filter</code> object.
+            @returns {Object} returns <code>this</code> so that calls can be chained.
+            */
+      post_filter: function (filter) {
+        if (filter == null) {
+          return query.filter;
+        }
+
+        if (!isFilter(filter)) {
+          throw new TypeError('Argument must be a Filter');
+        }
+
+        query.post_filter = filter.toJSON();
         return this;
       },
 
