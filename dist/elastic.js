4fda90033d482ff3daa23c27e097ec510b2b6691
alejandro <alemures@hotmail.com>
2016-04-13 16:54:56 +0100

    Added Children Aggregation

--- dist/elastic.js
+++ dist/elastic.js
@@ -1,6 +1,6 @@
-/*! elastic.js - v1.3.8 - 2015-11-02
+/*! elastic.js - v1.3.9 - 2016-04-13
  * https://github.com/fullscale/elastic.js
- * Copyright (c) 2015 FullScale Labs, LLC; Licensed MIT */
+ * Copyright (c) 2016 FullScale Labs, LLC; Licensed MIT */
 
 /**
  @namespace
@@ -3342,6 +3342,54 @@
 
     });
 
+  };
+
+  /**
+    @class
+    <p>A special single bucket aggregation that enables aggregating children
+    documents.</p>
+
+    @name ejs.ChildrenAggregation
+    @ejs aggregation
+    @borrows ejs.AggregationMixin.aggregation as aggregation
+    @borrows ejs.AggregationMixin.agg as agg
+    @borrows ejs.AggregationMixin._type as _type
+    @borrows ejs.AggregationMixin.toJSON as toJSON
+
+    @desc
+    <p>A special single bucket aggregation that enables aggregating children
+    documents.</p>
+
+    @param {String} name The name which be used to refer to this aggregation.
+
+    */
+  ejs.ChildrenAggregation = function (name) {
+
+    var
+      _common = ejs.AggregationMixin(name),
+      agg = _common.toJSON();
+
+    agg[name].children = {};
+
+    return extend(_common, {
+
+      /**
+      <p>Sets the children type.</p>
+
+      @member ejs.ChildrenAggregation
+      @param {String} children The children type value.
+      @returns {Object} returns <code>this</code> so that calls can be chained.
+      */
+      children: function (children) {
+        if (children == null) {
+          return agg[name].children.type;
+        }
+
+        agg[name].children.type = children;
+        return this;
+      }
+
+    });
   };
 
   /**
